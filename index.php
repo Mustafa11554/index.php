785d561584c3186d0de57360b9edf82b1e2c760b
Mustafa <mustafa01999@icloud.com>
2023-10-14 14:23:30 +0300

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Mustafa";
+$age = 14;
+$a = 5;
+$b = 10;
+$c = $a + $b;
+$d = $a - $b;
+$e = $a * $b; 
+$f = $a / $b;
+echo $c.' '.$d.' '.$e.' '.$f.'<br>'.'Hello ' . $name . ' your age is ' . $age;
+?>
